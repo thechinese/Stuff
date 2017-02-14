4887627ce9fd1b681c4a86bfbc4a56810215893d
thechinese <th3chinese@gmx.de>
2017-02-14 19:16:59 +0100

    Jo

---
+++ HelloYou/HelloYou/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloYou
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (RButton1.IsChecked == true)
+            {
+                MessageBox.Show("Option 1 was chosen.");
+            } else
+            {
+                if (RButton2.IsChecked == true)
+                {
+                    MessageBox.Show("Option 2 was chosen.");
+                }
+                                
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+    }
+}
